5c279cefedaea928c4ff021562fd0c280a76f768
AzazelLittle <nastechka.ignatchik@gmail.com>
2022-12-15 21:23:32 +0300

    feat: add localStorage and log out button

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../../components/context/context";
+import "./menu.css";
 
 const Menu = () => {
-    return (
-        <div>
-            Hi Lesha!
-        </div>
-    );
+  const authContext = useContext(AuthContext);
+  const clickHandlerLogOut = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (authContext) {
+      authContext.setIsUserAuth(false);
+    }
+  };
+  return (
+    <div>
+      <button className="log_out_button" onClick={clickHandlerLogOut}>
+        {" "}
+        Log out{" "}
+      </button>
+      Hi Lesha!
+    </div>
+  );
 };
 
 export default Menu;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Navigate, Route, Routes } from "react-router";
 
 import Login from "../pages/login/Login";
@@ -9,9 +9,19 @@ import Menu from "../pages/menu/Menu";
 
 
 const Router = () => {
-  const [isUserLogin, setIsUserLogin] = React.useState<boolean>(false);
+
+
+
+    const [isUserLogin, setIsUserLogin] = React.useState<boolean>(false);
   const [token, setToken] = React.useState<string>(" ");
-  const [isUserAuth, setIsUserAuth] = React.useState<boolean>(false);
+  const [isUserAuth, setIsUserAuth] = React.useState<boolean>( JSON.parse(localStorage.getItem('Auth') || ' '))
+
+    useEffect(() => {
+        localStorage.setItem('Auth', JSON.stringify(isUserAuth));
+    }, [isUserAuth]);
+
+
+
 
   return (
     <AuthContext.Provider
@@ -30,7 +40,9 @@ const Router = () => {
             <Route path="/*" element={<Navigate to="/Menu" replace />} />
             <Route path="/Menu" element={<Menu />} />
           </Routes>
-        ) : isUserLogin ? (
+        ) :
+
+            (isUserLogin ? (
           <Routes>
             <Route path="/*" element={<Navigate to="/Auth" replace />} />
             <Route path="/Auth" element={<Authorization />} />
@@ -39,8 +51,8 @@ const Router = () => {
           <Routes>
             <Route path="/Login" element={<Login />} />
             <Route path="/*" element={<Navigate to="/Login" replace />} />
-          </Routes>
-        )}
+          </Routes>))
+        }
       </BrowserRouter>
     </AuthContext.Provider>
   );
